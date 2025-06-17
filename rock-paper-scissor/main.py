f91313c70c10ff21b6b80b8abebd444375b94676
mrchiraggarg <gargchirag728@gmail.com>
2025-06-17 21:53:19 +0530

    commit

--- rock-paper-scissor/main.py
+++ rock-paper-scissor/main.py
@@ -5,11 +5,11 @@ computer_choice = random.choice(['rock', 'paper', 'scissors'])
 
 print("Welcome to Rock, Paper, Scissors!")
 print("You can choose from:")
-print("Select 1 For Rock")
-print("Select 2 For Paper")
-print("Select 3 For Scissors")
+print("Type 1 For Rock")
+print("Type 2 For Paper")
+print("Type 3 For Scissors")
 
-user_choice = int(input("Enter your choice (1, 2, or 3): "))
+user_choice = int(input("Enter your choice: "))
 
 if user_choice not in ['rock', 'paper', 'scissors']:
     print("Invalid choice. Please enter rock, paper, or scissors.")
