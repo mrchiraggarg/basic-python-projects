337b144c87c0d6107fc95d464e9b8919af1f71da
mrchiraggarg <gargchirag728@gmail.com>
2025-06-17 22:00:10 +0530

    commit

--- rock-paper-scissor/main.py
+++ rock-paper-scissor/main.py
@@ -12,10 +12,8 @@ try:
 
     user_choice = int(input("Enter your choice: "))
 
-
     if user_choice < 1 or user_choice > 3:
         print("Invalid choice. Please enter 1, 2, or 3.")
-
     else:
         if user_choice == 1:
             user_choice = 'rock'
